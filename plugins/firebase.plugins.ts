24310333acfa7ae9c7233da6fee61c6a05917461
joppe <joppe@touchwonders.com>
2023-07-31 14:52:17 +0200

    made the keys invisible to users

--- plugins/firebase.plugins.ts
+++ plugins/firebase.plugins.ts
@@ -19,12 +19,12 @@ export default defineNuxtPlugin((nuxtApp) => {
   if (process.server) {
     firebaseConfig = {
       apiKey: config.private.FIREBASE_API_KEY,
-      authDomain: config.FIREBASE_AUTH_DOMAIN,
-      projectId: config.FIREBASE_PROJECT_ID,
-      storageBucket: config.FIREBASE_STORAGE_BUCKET,
-      messagingSenderId: config.FIREBASE_MESSAGING_SENDER_ID,
-      appId: config.FIREBASE_APP_ID,
-      measurementId: config.FIREBASE_MEASUREMENT_ID
+      authDomain: config.private.FIREBASE_AUTH_DOMAIN,
+      projectId: config.private.FIREBASE_PROJECT_ID,
+      storageBucket: config.private.FIREBASE_STORAGE_BUCKET,
+      messagingSenderId: config.private.FIREBASE_MESSAGING_SENDER_ID,
+      appId: config.private.FIREBASE_APP_ID,
+      measurementId: config.private.FIREBASE_MEASUREMENT_ID
     };
   } else if (process.client) {
     firebaseConfig = {
