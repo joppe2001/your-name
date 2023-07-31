b5f83199094fe8f0f8dfdde5bbf78cb38725972c
joppe <joppe@touchwonders.com>
2023-07-31 14:32:22 +0200

    adding conditional to the server or client side api logging

--- plugins/firebase.plugins.ts
+++ plugins/firebase.plugins.ts
@@ -14,7 +14,19 @@ export default defineNuxtPlugin((nuxtApp) => {
 
   // Your web app's Firebase configuration
   // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
+let firebaseConfig = {};
+if (process.server) { 
+   firebaseConfig = {
+    apiKey: config.FIREBASE_API_KEY,
+    authDomain: config.FIREBASE_AUTH_DOMAIN,
+    projectId: config.FIREBASE_PROJECT_ID,
+    storageBucket: config.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: config.FIREBASE_MESSAGING_SENDER_ID,
+    appId: config.FIREBASE_APP_ID,
+    measurementId: config.FIREBASE_MEASUREMENT_ID
+  };
+} else if (process.client) {
+  firebaseConfig = {
     apiKey: config.public.FIREBASE_API_KEY,
     authDomain: config.public.FIREBASE_AUTH_DOMAIN,
     projectId: config.public.FIREBASE_PROJECT_ID,
@@ -23,6 +35,8 @@ export default defineNuxtPlugin((nuxtApp) => {
     appId: config.public.FIREBASE_APP_ID,
     measurementId: config.public.FIREBASE_MEASUREMENT_ID
   };
+}
+console.log('firebaseConfig', firebaseConfig);
 
   // Initialize Firebase
   const app = initializeApp(firebaseConfig);
