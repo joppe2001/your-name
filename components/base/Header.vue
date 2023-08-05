902e22f4ff280f6158d05c2552ed3d591261dc98
joppe <joppe@touchwonders.com>
2023-08-06 00:06:21 +0200

    trying to make the text more readable

--- components/base/Header.vue
+++ components/base/Header.vue
@@ -15,7 +15,7 @@
       <div v-else>
         <LogOut class="text-yn-golden text-m ml-4 p-1 hover:text-yn-sunset hover:bg-yn-lavender transition-all duration-200 rounded" />
         <nuxt-link to="/fanhangout/discussions" class="text-yn-golden text-m ml-4 p-1 hover:text-yn-sunset hover:bg-yn-lavender transition-all duration-200 rounded">
-          discussion
+          Posts
         </nuxt-link>
         <nuxt-link to="/account" class="text-yn-golden text-m ml-4 p-1 hover:text-yn-sunset hover:bg-yn-lavender transition-all duration-200 rounded">
           account
