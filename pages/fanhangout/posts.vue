9f0f0911be89237d754effb17a48963e01eb1519
joppe <joppe@touchwonders.com>
2023-08-06 02:22:38 +0200

    changed discussion page to posts

--- pages/fanhangout/posts.vue
+++ pages/fanhangout/posts.vue
@@ -52,7 +52,7 @@
                                         <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24">
                                             <path fill="currentColor"
                                                 d="m2.808 1.394l18.384 18.384l-1.414 1.415l-3.746-3.747L12 21.486l-8.478-8.493a6 6 0 0 1 .033-8.023L1.394 2.808l1.414-1.414Zm17.435 3.364a6 6 0 0 1 .236 8.235l-1.635 1.636L7.26 3.046a5.99 5.99 0 0 1 4.741 1.483a5.998 5.998 0 0 1 8.242.229Z" style="color: rgb(130, 130, 130)"
-                                                :style="{ fill: dislikedPostsIds.includes(post.id) ? 'blue' : '' }" />
+                                                :style="{ fill: dislikedPostsIds.includes(post.id) ? '#89cff0' : '' }" />
                                         </svg>
                                         <span
                                             style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%); transition: 0.3s ease-out">
