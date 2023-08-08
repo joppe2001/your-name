ebd9fb7a176b0e8513f830608445eb713871be35
joppe <joppe@touchwonders.com>
2023-08-08 09:38:36 +0200

    making sure image is loaded when home page is

--- components/MostLikedPost.vue
+++ components/MostLikedPost.vue
@@ -1,19 +1,30 @@
 <template>
-	<div class="flex items-center justify-center p-4">
-		<div class="max-w-4xl mx-auto text-center">
+	<div class="flex items-center justify-center p-4  mt-20 rounded">
+		<div class="post__content backdrop-blur-md w-1/2 p-5 mx-auto text-center">
 			<h2 class="text-2xl sm:text-4xl lg:text-5xl mb-4 sm:mb-6 font-extrabold text-yn-sunset">
 				Most Liked Post
 			</h2>
-			<h1>{{ highestLikedPost.title }}</h1>
-			<img :src="highestLikedPost.imageUrl" alt="Most liked post" class="mx-auto rounded shadow-lg" />
-			<p class="text-black text-sm sm:text-lg mb-4 sm:mb-8">
-				{{ highestLikedPost.content }}
-			</p>
-			<p>{{ highestLikedPost.likes }}</p>
+			<div class="post__text text-center">
+				<h1 class="post__title text-yn-night-sky text-2xl sm:text-3xl mb-4">
+					{{ highestLikedPost.title }}
+				</h1>
+				<p class="post__description text-yn-night-sky text-sm sm:text-lg mb-4 sm:mb-8">
+					{{ highestLikedPost.content }}
+				</p>
+			</div>
+			<div class="post__image">
+				<img :src="highestLikedPost.imageUrl" alt="Most liked post" class="mx-auto rounded shadow-lg"
+					loading="lazy" />
+			</div>
+			<div class="mb-4 sm:mb-8 mt-5 flex justify-center items-center">
+				<p class="text-black text-sm sm:text-lg w-1/3 bg-yn-sunset p-1 rounded">
+					likes: {{ highestLikedPost.likes }}
+				</p>
+			</div>
 		</div>
 	</div>
 </template>
-
+  
 <script setup>
 const { $getPosts } = useNuxtApp();
 
@@ -21,8 +32,24 @@ const posts = ref([]);
 const highestLikedPost = ref({});
 
 onMounted(async () => {
-    posts.value = await $getPosts();
-    highestLikedPost.value = posts.value.reduce((a, b) => a.likes > b.likes ? a : b);
+	posts.value = await $getPosts();
+	highestLikedPost.value = posts.value.reduce((a, b) => a.likes > b.likes ? a : b);
 });
-
 </script>
+
+<style scoped>
+.post__content {
+	border-radius: 5px;
+	background-color: rgba(255, 255, 255, 0.5);
+	backdrop-filter: blur(5px);
+}
+
+@media (max-width: 640px) {
+	.post__content {
+		background-color: rgba(255, 255, 255, 0.5);
+		backdrop-filter: blur(10px);
+		width: 95%;
+	}
+}
+</style>
+  
