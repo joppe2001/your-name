102bc2e75c4c77b09a96c95bd631a77ff5b2d576
joppe <joppe@touchwonders.com>
2023-08-08 14:50:09 +0200

    making sure image is loaded when home page is

--- components/MostLikedPost.vue
+++ components/MostLikedPost.vue
@@ -1,35 +1,48 @@
 <template>
 	<div class="flex items-center justify-center p-4  mt-10 rounded">
 		<div class="post__content backdrop-blur-md w-1/2 p-5 mx-auto text-center">
-			<h2 class="text-2xl sm:text-4xl lg:text-5xl mb-4 sm:mb-6 font-extrabold text-yn-sunset">
-				Most Liked Post
-			</h2>
-			<div class="post__text text-center">
-				<h1 class="post__title text-yn-night-sky text-2xl sm:text-3xl mb-4">
-					{{ highestLikedPost.title }}
-				</h1>
-				<p class="post__description text-yn-night-sky text-sm sm:text-lg mb-4 sm:mb-8">
-					{{ highestLikedPost.content }}
-				</p>
-			</div>
-			<div class="post__image">
-				<img :src="highestLikedPost.imageUrl" alt="Most liked post" class="mx-auto rounded shadow-lg"
-					loading="lazy" />
-			</div>
-			<div class="mb-4 sm:mb-8 mt-5 flex justify-center items-center">
-				<p class="text-black text-sm sm:text-lg w-1/3 bg-yn-sunset p-1 rounded">
-					likes: {{ highestLikedPost.likes }}
-				</p>
-			</div>
+			<button @click="showLogin">
+				<h2 class="text-2xl sm:text-4xl lg:text-5xl mb-4 sm:mb-6 font-extrabold text-yn-sunset">
+					Most Liked Post
+				</h2>
+				<div class="post__text text-center">
+					<h1 class="post__title text-yn-night-sky text-2xl sm:text-3xl mb-4">
+						{{ highestLikedPost.title }}
+					</h1>
+					<p class="post__description text-yn-night-sky text-sm sm:text-lg mb-4 sm:mb-8">
+						{{ highestLikedPost.content }}
+					</p>
+				</div>
+				<div class="post__image">
+					<img :src="highestLikedPost.imageUrl" alt="Most liked post" class="mx-auto rounded shadow-lg"
+						loading="lazy" />
+				</div>
+				<div class="mb-4 sm:mb-8 mt-5 flex justify-center items-center">
+					<p class="text-black text-sm sm:text-lg w-1/3 bg-yn-sunset p-1 rounded">
+						likes: {{ highestLikedPost.likes }}
+					</p>
+				</div>
+			</button>
 		</div>
 	</div>
 </template>
   
 <script setup>
+import { getAuth } from "firebase/auth";
 const { $getPosts } = useNuxtApp();
 
+const auth = getAuth();
 const posts = ref([]);
 const highestLikedPost = ref({});
+const router = useRouter();
+
+const showLogin = () => {
+	if (auth.currentUser != '' && window.confirm("see all posts?")) {
+		router.push("/fanhangout/posts");
+	} else if (window.confirm("Please login to view the posts") && auth.currentUser == ''){
+		router.push("/login");
+	}
+};
 
 onMounted(async () => {
 	posts.value = await $getPosts();
